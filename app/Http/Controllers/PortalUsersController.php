da4c551015e825ba413dcab6e450991b2ddcddad
Kevin Eduardo <kevimedu@gmail.com>
2016-10-22 02:09:37 -0200

    Fix :octopus:

--- app/Http/Controllers/PortalUsersController.php
+++ app/Http/Controllers/PortalUsersController.php
@@ -16,7 +16,7 @@ class PortalUsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('verifyteacher');
+        $this->middleware('verifyteacher:2');
     }
 
     /**
