2ddaf9a66348a8b6c1dca9a5caad70e96445b6eb
Kevin Eduardo <kevimedu@gmail.com>
2016-10-27 04:17:07 -0200

    Settings OIW
    OIW - On It's Way
    Commit só pra iniciar os trabalhos.

--- resources/lang/pt-br/messages.php
+++ resources/lang/pt-br/messages.php
@@ -32,6 +32,7 @@ return [
         'forgotp' => 'Esqueceu sua senha?',
         'resetp' => 'Resetar Senha',
         'sendresetp' => 'Enviar Link de Recuperação',
+        'config' => 'Configurações',
         'social' => [
             'fb' => 'Entrar com o Facebook',
         ],
---
+++ resources/views/portal/config.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.portal')
+
+@section('title', trans('messages.layout.config'))
+
+
+@section('javascripts')
+	@parent
+	<script src="{{ URL::asset('js/jquery.js') }}"></script>
+    <script>
+      $(document).ready (function(){
+            $(".alert-success").fadeTo(2200, 500).slideUp(500, function(){
+            $(".alert-success").slideUp(500);
+            });
+            $(".alert-danger").fadeTo(10000, 500).slideUp(500, function(){
+            $(".alert-danger").slideUp(500);
+            });
+      });
+    </script>
+@endsection
+
+@section('content')
+@if (count($errors) > 0)
+<div class="alert alert-dismissible alert-danger">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>@lang('messages.form.errors.post')</strong>
+  <ul>
+  @foreach ($errors->all() as $error)
+  	<li>{{ $error }}</li>
+  @endforeach
+  </ul>
+</div>
+@endif
+@if(isset($success))
+@if($success)
+<div class="alert alert-dismissible alert-success">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>@lang('messages.form.success.post.title')</strong> @lang('messages.form.success.post.msg')
+</div>
+@endif
+@endif
+@if(isset($editado))
+@if($editado)
+<div class="alert alert-dismissible alert-success">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>@lang('messages.form.success.edited.title')</strong> @lang('messages.form.success.edited.msg')
+</div>
+@endif
+@endif
+  <div class="col-md-9">
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+      <h3>HOME</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>Menu 1</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Menu 3</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
+  </div>
+  <vr>
+  <div class="col-md-3">
+  <ul class="nav nav-pills nav-stacked">
+    <li class="active"><a data-toggle="pill" href="#home">Home</a></li>
+    <li><a data-toggle="pill" href="#menu1">Menu 1</a></li>
+    <li><a data-toggle="pill" href="#menu2">Menu 2</a></li>
+    <li><a data-toggle="pill" href="#menu3">Menu 3</a></li>
+  </ul>
+</div>
+@endsection
