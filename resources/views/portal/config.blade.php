0cd601d03d5e9c0e401affac3f5aba36b438fdc3
KevinEduardo <kevimedu@gmail.com>
2016-11-04 16:54:58 -0200

    Gerenciar Turmas WOI 2%
    Código demo adicionado pra trabalhar em cima

--- resources/views/portal/config.blade.php
+++ resources/views/portal/config.blade.php
@@ -2,10 +2,63 @@
 
 @section('title', trans('messages.layout.config'))
 
+@section('styles')
+  @parent
+  <style type="text/css">
+    #outer-dropzone {
+  height: 140px;
+}
+
+#inner-dropzone {
+  height: 80px;
+}
+
+.dropzone {
+  background-color: #ccc;
+  border: dashed 4px transparent;
+  border-radius: 4px;
+  margin: 10px auto 30px;
+  padding: 10px;
+  width: 80%;
+  transition: background-color 0.3s;
+}
+
+.drop-active {
+  border-color: #aaa;
+}
+
+.drop-target {
+  background-color: #29e;
+  border-color: #fff;
+  border-style: solid;
+}
+
+.drag-drop {
+  display: inline-block;
+  min-width: 40px;
+  padding: 2em 0.5em;
+
+  color: #fff;
+  background-color: #29e;
+  border: solid 2px #fff;
+
+  -webkit-transform: translate(0px, 0px);
+          transform: translate(0px, 0px);
+
+  transition: background-color 0.3s;
+}
+
+.drag-drop.can-drop {
+  color: #000;
+  background-color: #4e4;
+}
+  </style>
+@endsection
 
 @section('javascripts')
 	@parent
   <script src="{{ URL::asset('jqueryui/jquery-ui.min.js') }}"></script>
+  <script src="{{ URL::asset('js/interact.min.js') }}"></script>
     <script>
       var triggered = false;
       $(document).ready (function(){
@@ -51,6 +104,89 @@
                 });
               });
       }
+      // target elements with the "draggable" class
+      interact('.draggable')
+        .draggable({
+          // enable inertial throwing
+          inertia: true,
+          // keep the element within the area of it's parent
+          restrict: {
+            restriction: "parent",
+            endOnly: true,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+          },
+          // enable autoScroll
+          autoScroll: true,
+
+          // call this function on every dragmove event
+          onmove: dragMoveListener,
+          // call this function on every dragend event
+          onend: function (event) {
+            var textEl = event.target.querySelector('p');
+
+            textEl && (textEl.textContent =
+              'moved a distance of '
+              + (Math.sqrt(event.dx * event.dx +
+                           event.dy * event.dy)|0) + 'px');
+          }
+        });
+
+        function dragMoveListener (event) {
+            var target = event.target,
+                // keep the dragged position in the data-x/data-y attributes
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            // translate the element
+            target.style.webkitTransform =
+            target.style.transform =
+              'translate(' + x + 'px, ' + y + 'px)';
+
+            // update the posiion attributes
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+          }
+
+        /* The dragging code for '.draggable' from the demo above
+         * applies to this demo as well so it doesn't have to be repeated. */
+
+        // enable draggables to be dropped into this
+        interact('.dropzone').dropzone({
+          // only accept elements matching this CSS selector
+          accept: '#yes-drop',
+          // Require a 75% element overlap for a drop to be possible
+          overlap: 0.75,
+
+          // listen for drop related events:
+
+          ondropactivate: function (event) {
+            // add active dropzone feedback
+            event.target.classList.add('drop-active');
+          },
+          ondragenter: function (event) {
+            var draggableElement = event.relatedTarget,
+                dropzoneElement = event.target;
+
+            // feedback the possibility of a drop
+            dropzoneElement.classList.add('drop-target');
+            draggableElement.classList.add('can-drop');
+            draggableElement.textContent = 'Dragged in';
+          },
+          ondragleave: function (event) {
+            // remove the drop feedback style
+            event.target.classList.remove('drop-target');
+            event.relatedTarget.classList.remove('can-drop');
+            event.relatedTarget.textContent = 'Dragged out';
+          },
+          ondrop: function (event) {
+            event.relatedTarget.textContent = 'Dropped';
+          },
+          ondropdeactivate: function (event) {
+            // remove active dropzone feedback
+            event.target.classList.remove('drop-active');
+            event.target.classList.remove('drop-target');
+          }
+        });
     </script>
 @endsection
 
@@ -127,8 +263,15 @@
       </form>
     </div>
     <div id="turmas" class="tab-pane fade">
-      <h3>Turmas</h3>
-      <p>TRETA.</p>
+      <div id="no-drop" class="draggable drag-drop"> #no-drop </div>
+
+      <div id="yes-drop" class="draggable drag-drop"> #yes-drop </div>
+
+      <div id="outer-dropzone" class="dropzone">
+        #outer-dropzone
+        <div id="inner-dropzone" class="dropzone">#inner-dropzone</div>
+       </div>
+
     </div>
     <div id="menu2" class="tab-pane fade">
       <h3>Menu 2</h3>
